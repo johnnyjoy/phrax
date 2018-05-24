c7eaa24fadcf14141a7badcc9f30e8a68393bc58
James Dornan <jdornan@stanford.edu>
2018-05-24 18:32:25 -0400

    Clearning up extra files in the path test folder.

--- path/test/testinclude.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once '..' .  DIRECTORY_SEPARATOR . '..' .  DIRECTORY_SEPARATOR . '_.php';
-
-$dbtestdir  = (isset($_ENV['TMP_DIR']) ? $_ENV['TMP_DIR'] : '/tmp');
-
-$opts['handler'] = 'qdbm';
-?>
